3dd59f6e8ca0bbb6302f319b0ceaae3a072430b9
kcunanan <kevin.a.cunanan@gmail.com>
2016-01-26 14:40:20 -0800

    commented and added random.txt. Also edited readme slightly

--- lab1/README.md
+++ lab1/README.md
@@ -1,5 +1,7 @@
 # Lab1 CMC-CS181
 
-To compile this file simply run *python lab1.py file.txt*.
-* file.txt is the filename that we want to run the entropy on.
+To compile this file simply run `python lab1.py filename.txt`.
+* filename.txt is the filename that we want to run the entropy on.
 * All information will be outputted to the command line in pretty JSON.
+  * Key values are organized in alphabetical order
+  * Entropy is provided last
--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,11 +1,15 @@
-import json
-import math
-import sys
+# Kevin Cunanan
+# username: kcunanan
+# Lab1
+
+import json # json help
+import math # log functions
+import sys # necessary to call read file
 
 dict1 = {}
 with open(str(sys.argv[1]), 'rb') as textFile:
     for line in textFile:
-        chars = list(unicode(line, errors='ignore'))
+        chars = list(unicode(line, errors='ignore')) # eliminates weird beginning characters that break the ASCII conversion
         for val in chars:
             if val in dict1:
                 dict1[val] += 1
@@ -13,11 +17,11 @@ with open(str(sys.argv[1]), 'rb') as textFile:
                 dict1[val] = 1
 total = 0
 values = []
-for key, value in dict1.iteritems():
-    if len(key) == 1:
+for key, value in dict1.iteritems(): #iterate through items in dict1
+    if len(key) == 1: # if not it's a non-printable we've gone thru
         total += value
         values.append(value)
-        dict1[key] = str(value)
+        dict1[key] = str(value) # convert value to string for JSON output
         if ord(key) > 32:
             key = key
         else:
@@ -25,11 +29,11 @@ for key, value in dict1.iteritems():
             del dict1[key]
 dict1['total'] = str(total)
 entropy = 0
-for a in range(len(values)):
+for a in range(len(values)): # iterate thru array, summation entropy calculations
     values[a] = (values[a]/float(total))
     values[a] = (values[a] * math.log(values[a], 2))
     entropy += values[a]
 entropy_dict = {}
 entropy_dict['entropy'] = str(-entropy)
-print json.dumps(dict1, sort_keys=True, indent=4, separators=(',',': '))
+print json.dumps(dict1, sort_keys=True, indent=4, separators=(',',': ')) # for pretty JSON output
 print json.dumps(entropy_dict, sort_keys=True, indent=4, separators=(',',': '))
