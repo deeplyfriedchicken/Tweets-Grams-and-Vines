316a943ff0b44010001effab6662f64a2ee3b32d
kcunanan <kevin.a.cunanan@gmail.com>
2016-01-28 21:20:13 -0800

    fixed errors based on prof goodney's json comment

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -9,7 +9,7 @@ import sys # necessary to call read file
 dict1 = {}
 with open(str(sys.argv[1]), 'rb') as textFile:
     for line in textFile:
-        chars = list(unicode(line, errors='ignore')) # eliminates weird beginning characters that break the ASCII conversion
+        chars = list(line)
         for val in chars:
             if val in dict1:
                 dict1[val] += 1
@@ -21,12 +21,11 @@ for key, value in dict1.iteritems(): #iterate through items in dict1
     if len(key) == 1: # if not it's a non-printable we've gone thru
         total += value
         values.append(value)
-        dict1[key] = str(value) # convert value to string for JSON output
-        if ord(key) > 32:
-            key = key
-        else:
+        if ord(key) < 32 or ord(key) >= 127:
             dict1[str(hex(ord(key)))] = dict1[key]
             del dict1[key]
+for key, value in dict1.iteritems():
+    dict1[key] = str(value) # convert value to string for JSON output
 dict1['total'] = str(total)
 entropy = 0
 for a in range(len(values)): # iterate thru array, summation entropy calculations
