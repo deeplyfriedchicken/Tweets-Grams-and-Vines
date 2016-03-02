432ae78f96b8833563d2b9f618bfe3d9b81cec33
kcunanan <kevin.a.cunanan@gmail.com>
2016-03-02 08:30:52 -0800

    I FIGURED IT OUT. Another one liner mistake...

--- lab2/lab2-decode.py
+++ lab2/lab2-decode.py
@@ -8,18 +8,20 @@ w = open(str('uncompressed.bin'), "wb")
 byte = f.read(2)
 char = f.read(1)
 buf = []
+copy_idx = 0
 while byte != "":
+    if len(buf) > 1024:
+        copy_idx = len(buf) - 1024
     offset_and_length = unpack(">H", byte)
     offset = offset_and_length[0] >> 6
     length = offset_and_length[0] - (offset << 6)
-    print "Length "+str(length)
-    print "Offset "+str(offset)
+    print "Length " + str(length)
+    print "Offset " + str(offset)
     if length == 0:
         buf.append(char)
     else:
-        copy_idx = offset
         for i in range (0, length):
-            buf.append(buf[copy_idx + i])
+            buf.append(buf[copy_idx + offset + i])
         buf.append(char)
     byte = f.read(2)
     char = f.read(1)
--- lab2/lab2-encode.py
+++ lab2/lab2-encode.py
@@ -23,7 +23,6 @@ def LZ77_search(search, look_ahead):
     search_pointer = ls
 
     for i in range (0, ls):
-        offset = 0
         length = 0
         while buf[i+length] == buf[search_pointer+length]:
             length += 1
@@ -39,25 +38,24 @@ def main():
     MAX_SEARCH = 1024
     MAX_LOOKAHEAD = 64
     r = open(str(sys.argv[1]) ,"rb")
-    inputme = r.read(MAX_SEARCH+MAX_LOOKAHEAD)
+    inputme = r.read()
     search_idx = 0
     lookahead_idx = 0
-    search = inputme[search_idx:lookahead_idx]
-    lookahead = inputme[lookahead_idx:lookahead_idx+MAX_LOOKAHEAD]
     f = open('compressed.bin', 'wb')
-    while len(lookahead) != 0:
+    while lookahead_idx < len(inputme):
+        search = inputme[search_idx:lookahead_idx]
+        lookahead = inputme[lookahead_idx:lookahead_idx + MAX_LOOKAHEAD]
         (offset, length, char) = LZ77_search(search, lookahead)
-        lookahead_idx += length + 1
         shifted_offset = offset << 6
         offset_and_length = shifted_offset + length
         ol_bytes = pack('>H', offset_and_length)
         f.write(ol_bytes)
         f.write(char)
-        print "<"+str(offset)+","+str(length)+","+str(char)+">"
-        inputme = inputme + r.read(length+1)
-        if (lookahead_idx - search_idx) > MAX_SEARCH:
-            search_idx = lookahead_idx - MAX_SEARCH
-        search = inputme[search_idx:lookahead_idx]
-        lookahead = inputme[lookahead_idx:lookahead_idx+MAX_LOOKAHEAD]
+        print (offset, length, char)
+        # inputme = inputme + r.read(MAX_LOOKAHEAD)
+        lookahead_idx += length + 1
+        search_idx = lookahead_idx - MAX_SEARCH
+        if search_idx < 0:
+            search_idx = 0
     f.close
 main()
